f39f05dd7ccc4ff72e598b6b84f67ffaacd58d92
cyeond <cyeond@gmail.com>
2023-12-05 10:15:55 +0900

    initial commit

---
+++ UrlSessionUnitTestSample/UrlSessionUnitTestSampleTests/UrlSessionUnitTestSampleTests.swift
@@ -0,0 +1,20 @@
+//
+//  UrlSessionUnitTestSampleTests.swift
+//  UrlSessionUnitTestSampleTests
+//
+//  Created by YD on 12/5/23.
+//
+
+import XCTest
+@testable import UrlSessionUnitTestSample
+
+final class UrlSessionUnitTestSampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
